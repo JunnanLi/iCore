cc513371d7799bd6712708dbc765c7bc4df83c4a
JunnanLi <30546476+JunnanLi@users.noreply.github.com>
2020-02-09 22:53:15 +0800

    Update testbench_for_iCore.v

--- hardware/testbench_for_iCore.v
+++ hardware/testbench_for_iCore.v
@@ -1,15 +1,16 @@
 /*
- *  iCore -- A hardware packet processing pipeline integrated with a in-line RISC-V Core
- *  Copyright (C) 2019-2020 Junnan Li <lijunnan@nudt.edu.cn>
+ *  iCore_hardware -- Hardware for TuMan RISC-V (RV32I) Processor Core.
  *
- *  Permission to use, copy, modify, and/or distribute this code for any purpose with or
- *   without fee is hereby granted, provided that the above copyright notice and this 
- *   permission notice appear in all copies.
+ *  Copyright (C) 2019-2020 Junnan Li <lijunnan@nudt.edu.cn>.
+ *  Copyright and related rights are licensed under the MIT license.
  *
- *	Function description: Test_bench.
+ *	Data: 2020.01.01
+ *	Description: Test_bench
  */
 `timescale 1 ns / 1 ps
-/**	Please toggle following comment (i.e., `define MODELSIM) if you use ModelSim **/
+/**	Please toggle following comment (i.e., `define MODELSIM) if you use 
+ ** ModelSim 
+ **/
 // `define MODELSIM
 
 module test_for_icore();
@@ -32,8 +33,10 @@ module test_for_icore();
 	end
 `endif
 
-	wire 		data_in_valid, data_out_valid;
-	wire[133:0]	data_in, data_out;
+	wire 		data_out_valid, data_in_valid_temp;
+	wire[133:0]	data_out, data_in_temp;
+	reg 		data_in_valid;
+	reg [133:0]	data_in;
 
 um UM(
 	.clk(clk),
@@ -48,42 +51,70 @@ um UM(
 	.pktout_data(data_out),
 	.pktout_data_valid(),
 	.pktout_data_valid_wr(),
-	.pktout_ready(1'b1),
-
-	.dma2um_data(),
-	.dma2um_data_wr(),
-	.um2dma_ready(),
-	.um2dma_data(),
-	.um2dma_data_wr(),
-	.dma2um_ready(),
-
-	.um2me_key_wr(),
-	.um2me_key_valid(),
-	.um2match_key(),
-	.um2me_ready(),
-
-	.me2um_id_wr(),
-	.match2um_id(),
-	.um2match_gme_alful(),
-
-	.ctrl_valid(),
-	.ctrl2um_cs_n(),
-	.um2ctrl_ack_n(),
-	.ctrl_cmd(),
-	.ctrl_datain(),
-	.ctrl_addr(),
-	.ctrl_dataout()
+	.pktout_ready(1'b1)
 );
 
 gen_data genData(
 	.clk(clk),
 	.resetn(resetn),
 
-	.data_in_valid(data_in_valid),
-	.data_in(data_in),
+	.data_in_valid(data_in_valid_temp),
+	.data_in(data_in_temp),
 	.data_out_valid(data_out_valid),
 	.data_out(data_out)
 );
+
+
+	reg [31:0]	count;
+
+always @(posedge clk or negedge resetn) begin
+	if (!resetn) begin
+		// reset
+		data_in_valid <= 1'b0;
+		data_in <= 134'b0;
+		count <= 32'b0;
+	end
+	else begin
+		count <= count + 32'd1;
+		if(count[12] == 1'b1) begin
+			case(count[2:0])
+				3'd0: begin
+					data_in_valid <= 1'b1;
+					data_in <= {2'b01,4'b0,128'b0};
+				end
+				3'd1: begin
+					data_in_valid <= 1'b1;
+					data_in <= {2'b11,4'b0,128'b0};
+				end
+				3'd2: begin
+					data_in_valid <= 1'b1;
+					data_in <= {2'b11,4'b0,48'h8c16_4549_2501,48'h8c16_4549_25ac,32'h08004500};
+				end
+				3'd3: begin
+					data_in_valid <= 1'b1;
+					data_in <= {2'b11,4'b0,48'h002c_0000_4000,48'h4006_863e_cac5,32'h0f82cac5};
+				end
+				3'd4: begin
+					data_in_valid <= 1'b1;
+					data_in <= {2'b11,4'b0,48'h0f81_1389_6001,48'hd332_7179_5602,32'h6cbf6012};
+				end
+				3'd5: begin
+					data_in_valid <= 1'b1;
+					data_in <= {2'b10,4'b0,48'hfaf0_b4ac_0000,
+						48'h0204_05b4_0000, 32'b0};
+				end
+				3'd6: begin
+					data_in_valid <= 1'b0;
+					count <= count;
+				end
+			endcase
+		end
+		else begin
+			data_in <= data_in_temp;
+			data_in_valid <= data_in_valid_temp;
+		end
+	end
+end
 
 endmodule
 
